b5ac86c3b3367f68d3becbca881cbf2a0d4af835
Alejandro Stuardo <alejandro.stuardoalarcon@gmail.com>
2024-01-20 12:41:30 -0300

    add registro logic

--- src/components/Registro.jsx
+++ src/components/Registro.jsx
@@ -5,19 +5,26 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFacebook } from '@fortawesome/free-brands-svg-icons'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import Title from "./Title"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 const faceLogo = <FontAwesomeIcon icon={faFacebook} />
 const gitLogo = <FontAwesomeIcon icon={faGithub} />
 const linkLogo = <FontAwesomeIcon icon={faLinkedin} />
 
-const Registro = () => {
+const Registro = ({alert, setAlert, color, msg}) => {
   return (
     <>
+    <Title/>
     <SocialButton logo={faceLogo}/>
     <SocialButton logo={gitLogo}/>
     <SocialButton logo={linkLogo}/>
-    <Formulario />
-    <Alert/>
+    <Formulario setAlert={setAlert}  />
+    <Alert 
+    color={color}
+    alert={alert}
+    msg={msg}
+    />
     </>
   )
 }
