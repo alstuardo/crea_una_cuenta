6981e3dd3a74a961fddc17729cd69367c57b64ab
Alejandro Stuardo <alejandro.stuardoalarcon@gmail.com>
2024-01-20 12:41:13 -0300

    add formulario logic

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,8 +1,107 @@
-const Formulario = () => {
+import { useState } from "react"
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './Formulario.css'
+
+const Formulario = ({setAlert}) => {
+        const [formData, setFormData] = useState({
+          name:'',
+          email:'',
+          password:'',
+          confirmPassword:''
+        });
+        
+        const handleSubmit = (e) => {
+          e.preventDefault();
+      
+          const {name, email, password, confirmPassword} = formData;
+          const validateInput = !name || !email || !password || !confirmPassword;
+          const validatePassword = password !== confirmPassword;
+      
+          validateInput ? setAlert({
+            error: true,
+            msg: 'Debes llenar todos los campos',
+            color: 'danger'
+          }) 
+          : setAlert ({
+            error: false,
+            msg: 'Haz creado tu cuenta',
+            color: 'success'
+          })
+      
+          if (validatePassword){
+            setAlert({
+            error: true,
+            msg: 'Las contraseñas no coinciden',
+            color: 'danger'
+            })
+            return;
+          }
+         
+          setFormData({
+          name:'',
+          email:'',
+          password:'',
+          confirmPassword:''
+          })
+        }
+      
+        const handleChange = (e) => {
+          setFormData({...formData, [e.target.name]:e.target.value}) 
+        }
+
+
   return (
-    <>
-    Formulario
-    </>
+    <Form onSubmit={handleSubmit}>
+    <Form.Group className="mb-3" controlId="formBasicInput">
+      <Form.Label className='subtitle'>o usa tu email para registrarte</Form.Label>
+      <Form.Control 
+      className='input' 
+      type="text" 
+      placeholder="Nombre" 
+      name='name'
+      onChange={handleChange}
+      value={formData.name} 
+      />
+    </Form.Group>
+
+    <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Control 
+      className='input' 
+      type="email" 
+      placeholder="Correo electrónico"
+      name='email'
+      onChange={handleChange}
+      value={formData.email}
+      />
+    </Form.Group>
+
+    <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Control 
+      className='input' 
+      type="password" 
+      placeholder="Contraseña"
+      name='password'
+      onChange={handleChange}
+      value={formData.password}
+      />
+    </Form.Group>
+
+    <Form.Group className="mb-3" controlId="formBasicPassword2">
+      <Form.Control 
+      className='input' 
+      type="password" 
+      placeholder="Confirma tu contraseña"
+      name='confirmPassword'
+      onChange={handleChange}
+      value={formData.confirmPassword}
+      />
+    </Form.Group>
+  
+    <Button variant="success" type="submit">
+      Registrarse
+    </Button>
+  </Form>
   )
 }
 
